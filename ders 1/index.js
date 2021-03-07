87eee196907732d1bceaba2912b62ce8cb0e27a6
gulsahatalan <atalangulsah@gmail.com>
2021-03-07 21:06:09 +0100

    add calculator

--- ders 1/index.js
+++
@@ -1,9 +0,0 @@
-//yeni modul sistemi
-//import merhaba from './functions'
-
-//eski modul sistem
-
-const {merhaba , gulegule} = require("./functions");
-console.log("node'dan merhabalar");
-merhaba();
-gulegule();
